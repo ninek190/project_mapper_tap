70f15ff4e0cca8a6fa12d49872523486e9f60dba
ninek190 <nino.cajnkar1@gmail.com>
2025-01-17 18:19:54 +0100

    updated filter functions
    new filter functions for distance to point and line, good results

--- project_mapper.jl
+++ project_mapper.jl
@@ -1,6 +1,6 @@
 import Clustering
 import Distances
-import LinearAlgebra: norm, det, inv
+import LinearAlgebra: norm, det, inv, cross
 import Statistics: cov, mean, std
 import MultivariateStats
 import PlyIO
@@ -80,6 +80,43 @@ function z_coordinate(X::AbstractMatrix{<:Real}; kwargs...)
 end
 
 # Filtering functions
+
+function distance_points_to_line(X::AbstractMatrix{<:Real}; kwargs...)
+    p0 = [0.0,0.0,0.0]
+    d = [1.0,1.0,1.0]
+    for (a,b) in kwargs
+        if a == :p0
+            p0 = b
+        elseif a == :d
+            d = b
+        end
+    end
+    denom = norm(d)
+    N = size(X, 1)
+    distances = similar(X, N)
+
+    for i in 1:N
+        point = X[i, :]
+        numerator = norm(cross(point .- p0, d))
+        distances[i] = numerator / denom
+    end
+    return distances
+end
+
+function distance_points_to_point(X::AbstractMatrix{<:Real}; kwargs...)
+    p1 = [0.0,0.0,0.0]
+    for (a, b) in kwargs
+        if a == :p1
+            p1 = b
+        end
+    end
+    N = size(X, 1)
+    distances = Vector{Float64}(undef, N)
+    for i in 1:N
+        distances[i] = norm(X[i, :] .- p1)
+    end
+    return distances
+end
 
 """
 2 filter functions, first one useful for gaussian clusters to highlight core cluster regions
@@ -369,13 +406,30 @@ using Plots
     end
 end
 
-# chair_16
+function average_point(X::AbstractMatrix{<:Real}; kwargs...)
+    N = size(X, 1)
+    x_val = 0.0
+    y_val = 0.0
+    z_val = 0.0
+    for i in 1:N
+        point = X[i,:]
+        x_val += point[1]
+        y_val += point[2]
+        z_val += point[3]
+    end
+    x_avg = x_val / N
+    y_avg = y_val / N
+    z_avg = z_val / N
+    return [x_avg, y_avg, z_avg]
+end
+
+
+# chair_16, z-coordinate
 
 chair = get_abstract_matrix("data/non-articulated/chairs/chairsPly/b16.ply")
 chair_mapper = mapper(chair, z_coordinate, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2)
-println("no_of_patches: ", length(chair_mapper.found_patches))
 mapper_chair_plot = plot(chair_mapper)
-savefig(mapper_chair_plot, "mapper_chair")
+savefig(mapper_chair_plot, "mapper_chair_z_coordinate")
 
 centers_chair = chair_mapper.centers_of_patches
 using Distances
@@ -384,15 +438,76 @@ using Ripserer
 
 result_chair = ripserer(dist_matrix_chair)
 fig_chair = plot(result_chair, title="Persistence Diagrams for Mapper Complex")
-savefig(fig_chair, "persistence_diagram_chair")
+savefig(fig_chair, "persistence_diagram_chair_z_coordinate")
 
-# ant_16
+# chair_16, distance to line x=y=z
+
+chair_mapper2 = mapper(chair, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2)
+mapper_chair_plot2 = plot(chair_mapper2)
+savefig(mapper_chair_plot2, "mapper_chair_distance_to_line_x_y_z")
+
+centers_chair2 = chair_mapper2.centers_of_patches
+using Distances
+dist_matrix_chair2 = pairwise(Euclidean(), centers_chair2)
+using Ripserer
+
+result_chair2 = ripserer(dist_matrix_chair2)
+fig_chair2 = plot(result_chair2, title="Persistence Diagrams for Mapper Complex")
+savefig(fig_chair2, "persistence_diagram_chair_distance_to_line_x_y_z")
+
+# chair_16, distance to line x
+
+chair_mapper3 = mapper(chair, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2, d=[1.0,0.0,0.0])
+mapper_chair_plot3 = plot(chair_mapper3)
+savefig(mapper_chair_plot3, "mapper_chair_distance_to_line_x")
+
+centers_chair3 = chair_mapper3.centers_of_patches
+using Distances
+dist_matrix_chair3 = pairwise(Euclidean(), centers_chair3)
+using Ripserer
+
+result_chair3 = ripserer(dist_matrix_chair3)
+fig_chair3 = plot(result_chair3, title="Persistence Diagrams for Mapper Complex")
+savefig(fig_chair3, "persistence_diagram_chair_distance_to_line_x")
+
+# chair_16, distance to line y
+
+chair_mapper4 = mapper(chair, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2, d=[0.0,1.0,0.0])
+mapper_chair_plot4 = plot(chair_mapper4)
+savefig(mapper_chair_plot4, "mapper_chair_distance_to_line_y")
+
+centers_chair4 = chair_mapper4.centers_of_patches
+using Distances
+dist_matrix_chair4 = pairwise(Euclidean(), centers_chair4)
+using Ripserer
+
+result_chair4 = ripserer(dist_matrix_chair4)
+fig_chair4 = plot(result_chair4, title="Persistence Diagrams for Mapper Complex")
+savefig(fig_chair4, "persistence_diagram_chair_distance_to_line_y")
+
+# chair_16, distance to avg_point
+
+chair_avg_point = average_point(chair)
+chair_mapper5 = mapper(chair, distance_points_to_point, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2, p1=chair_avg_point)
+mapper_chair_plot5 = plot(chair_mapper5)
+savefig(mapper_chair_plot5, "mapper_chair_distance_to_avg_point")
+
+centers_chair5 = chair_mapper5.centers_of_patches
+using Distances
+dist_matrix_chair5 = pairwise(Euclidean(), centers_chair5)
+using Ripserer
+
+result_chair5 = ripserer(dist_matrix_chair5)
+fig_chair5 = plot(result_chair5, title="Persistence Diagrams for Mapper Complex")
+savefig(fig_chair5, "persistence_diagram_chair_distance_to_avg_point")
+
+# ant_16, z-coordinate
 
 ant = get_abstract_matrix("data/articulated/ants/antsPly/16.ply")
 ant_mapper = mapper(ant, z_coordinate, balanced_cover, cluster_function, silhouette_method, no_of_intervals = 7, allowed_overlap = 0.7, ϵ = 1.0, measurement_func = point_distances2)
 println("no_of_patches: ", length(ant_mapper.found_patches))
 mapper_ant_plot = plot(ant_mapper)
-savefig(mapper_ant_plot, "mapper_ant")
+savefig(mapper_ant_plot, "mapper_ant_z_coordinate")
 
 centers_ant = ant_mapper.centers_of_patches
 using Distances
@@ -401,15 +516,80 @@ using Ripserer
 
 result_ant = ripserer(dist_matrix_ant)
 fig_ant = plot(result_ant, title="Persistence Diagrams for Mapper Complex")
-savefig(fig_ant, "persistence_diagram__ant")
+savefig(fig_ant, "persistence_diagram_ant_z_coordinate")
 
-# airplane_16
+# ant_16, distance to line x=y=z
+
+ant_mapper2 = mapper(ant, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, no_of_intervals = 7, allowed_overlap = 0.7, ϵ = 1.0, measurement_func = point_distances2)
+println("no_of_patches: ", length(ant_mapper2.found_patches))
+mapper_ant_plot2 = plot(ant_mapper2)
+savefig(mapper_ant_plot2, "mapper_ant_distance_to_line_x_y_z")
+
+centers_ant2 = ant_mapper2.centers_of_patches
+using Distances
+dist_matrix_ant2 = pairwise(Euclidean(), centers_ant2)
+using Ripserer
+
+result_ant2 = ripserer(dist_matrix_ant2)
+fig_ant2 = plot(result_ant2, title="Persistence Diagrams for Mapper Complex")
+savefig(fig_ant2, "persistence_diagram_ant_distance_to_line_x_y_z")
+
+# ant_16, distance to line x
+
+ant_mapper3 = mapper(ant, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, d = [1.0,0.0,0.0], no_of_intervals = 7, allowed_overlap = 0.7, ϵ = 1.0, measurement_func = point_distances2)
+println("no_of_patches: ", length(ant_mapper3.found_patches))
+mapper_ant_plot3 = plot(ant_mapper3)
+savefig(mapper_ant_plot3, "mapper_ant_distance_to_line_x")
+
+centers_ant3 = ant_mapper3.centers_of_patches
+using Distances
+dist_matrix_ant3 = pairwise(Euclidean(), centers_ant3)
+using Ripserer
+
+result_ant3 = ripserer(dist_matrix_ant3)
+fig_ant3 = plot(result_ant3, title="Persistence Diagrams for Mapper Complex")
+savefig(fig_ant3, "persistence_diagram_ant_distance_to_line_x")
+
+# ant_16, distance to line y
+
+ant_mapper4 = mapper(ant, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, d = [0.0,1.0,0.0], no_of_intervals = 7, allowed_overlap = 0.7, ϵ = 1.0, measurement_func = point_distances2)
+println("no_of_patches: ", length(ant_mapper4.found_patches))
+mapper_ant_plot4 = plot(ant_mapper4)
+savefig(mapper_ant_plot4, "mapper_ant_distance_to_line_y")
+
+centers_ant4 = ant_mapper4.centers_of_patches
+using Distances
+dist_matrix_ant4 = pairwise(Euclidean(), centers_ant4)
+using Ripserer
+
+result_ant4 = ripserer(dist_matrix_ant4)
+fig_ant4 = plot(result_ant4, title="Persistence Diagrams for Mapper Complex")
+savefig(fig_ant4, "persistence_diagram_ant_distance_to_line_y")
+
+# ant_16, distance to average point
+
+ant_avg_point = average_point(ant)
+ant_mapper5 = mapper(ant, distance_points_to_point, balanced_cover, cluster_function, silhouette_method, p1 = ant_avg_point, no_of_intervals = 7, allowed_overlap = 0.7, ϵ = 1.0, measurement_func = point_distances2)
+println("no_of_patches: ", length(ant_mapper5.found_patches))
+mapper_ant_plot5 = plot(ant_mapper5)
+savefig(mapper_ant_plot5, "mapper_ant_distance_to_avg_point")
+
+centers_ant5 = ant_mapper5.centers_of_patches
+using Distances
+dist_matrix_ant5 = pairwise(Euclidean(), centers_ant5)
+using Ripserer
+
+result_ant5 = ripserer(dist_matrix_ant5)
+fig_ant5 = plot(result_ant5, title="Persistence Diagrams for Mapper Complex")
+savefig(fig_ant5, "persistence_diagram_ant_distance_to_avg_point")
+
+# airplane_16, z-coordinate
 
 airplane = get_abstract_matrix("data/non-articulated/airplanes/airplanesPly/b16.ply")
 airplane_mapper = mapper(airplane, z_coordinate, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2)
 println("no_of_patches: ", length(airplane_mapper.found_patches))
 mapper_airplane_plot = plot(airplane_mapper)
-savefig(mapper_airplane_plot, "mapper_airplane")
+savefig(mapper_airplane_plot, "mapper_airplane_z_coordinate")
 
 centers_airplane = airplane_mapper.centers_of_patches
 using Distances
@@ -418,32 +598,14 @@ using Ripserer
 
 result_airplane = ripserer(dist_matrix_airplane)
 fig_airplane = plot(result_airplane, title="Persistence Diagrams for Mapper Complex")
-savefig(fig_airplane, "persistence_diagram_airplane")
+savefig(fig_airplane, "persistence_diagram_airplane_z_coordinate")
 
-# airplane_1
+# airplane_16, distance to line x=y=z
 
-airplane1 = get_abstract_matrix("data/non-articulated/airplanes/airplanesPly/b1.ply")
-airplane_mapper1 = mapper(airplane1, z_coordinate, balanced_cover, cluster_function, no_of_intervals=7, allowed_overlap=0.5, silhouette_method, ϵ = 1.0, measurement_func = point_distances2)
-println("no_of_patches: ", length(airplane_mapper1.found_patches))
-mapper_airplane1_plot = plot(airplane_mapper1)
-savefig(mapper_airplane1_plot, "mapper_airplane1")
-
-centers_airplane1 = airplane_mapper1.centers_of_patches
-using Distances
-dist_matrix_airplane1 = pairwise(Euclidean(), centers_airplane1)
-using Ripserer
-
-result_airplane1 = ripserer(dist_matrix_airplane1)
-fig_airplane1 = plot(result_airplane1, title="Persistence Diagrams for Mapper Complex")
-savefig(fig_airplane1, "persistence_diagram_airplane1")
-
-# airplane_2
-
-airplane2 = get_abstract_matrix("data/non-articulated/airplanes/airplanesPly/b2.ply")
-airplane_mapper2 = mapper(airplane2, z_coordinate, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2)
+airplane_mapper2 = mapper(airplane, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2)
 println("no_of_patches: ", length(airplane_mapper2.found_patches))
-mapper_airplane2_plot = plot(airplane_mapper2)
-savefig(mapper_airplane2_plot, "mapper_airplane2")
+mapper_airplane_plot2 = plot(airplane_mapper2)
+savefig(mapper_airplane_plot2, "mapper_airplane_distance_to_x_y_z")
 
 centers_airplane2 = airplane_mapper2.centers_of_patches
 using Distances
@@ -452,15 +614,14 @@ using Ripserer
 
 result_airplane2 = ripserer(dist_matrix_airplane2)
 fig_airplane2 = plot(result_airplane2, title="Persistence Diagrams for Mapper Complex")
-savefig(fig_airplane2, "persistence_diagram_airplane2")
+savefig(fig_airplane2, "persistence_diagram_airplane_distance_to_x_y_z")
 
-# airplane_3
+# airplane_16, distance to line x
 
-airplane3 = get_abstract_matrix("data/non-articulated/airplanes/airplanesPly/b3.ply")
-airplane_mapper3 = mapper(airplane3, z_coordinate, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2)
+airplane_mapper3 = mapper(airplane, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, d = [1.0,0.0,0.0], ϵ = 1.0, measurement_func = point_distances2)
 println("no_of_patches: ", length(airplane_mapper3.found_patches))
-mapper_airplane3_plot = plot(airplane_mapper3)
-savefig(mapper_airplane3_plot, "mapper_airplane3")
+mapper_airplane_plot3 = plot(airplane_mapper3)
+savefig(mapper_airplane_plot3, "mapper_airplane_distance_to_x")
 
 centers_airplane3 = airplane_mapper3.centers_of_patches
 using Distances
@@ -469,15 +630,14 @@ using Ripserer
 
 result_airplane3 = ripserer(dist_matrix_airplane3)
 fig_airplane3 = plot(result_airplane3, title="Persistence Diagrams for Mapper Complex")
-savefig(fig_airplane3, "persistence_diagram_airplane3")
+savefig(fig_airplane3, "persistence_diagram_airplane_distance_to_x")
 
-# airplane_4
+# airplane_16, distance to line y
 
-airplane4 = get_abstract_matrix("data/non-articulated/airplanes/airplanesPly/b4.ply")
-airplane_mapper4 = mapper(airplane4, z_coordinate, balanced_cover, cluster_function, silhouette_method, ϵ = 1.0, measurement_func = point_distances2)
+airplane_mapper4 = mapper(airplane, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, d = [0.0,1.0,0.0], ϵ = 1.0, measurement_func = point_distances2)
 println("no_of_patches: ", length(airplane_mapper4.found_patches))
-mapper_airplane4_plot = plot(airplane_mapper4)
-savefig(mapper_airplane4_plot, "mapper_airplane4")
+mapper_airplane_plot4 = plot(airplane_mapper4)
+savefig(mapper_airplane_plot4, "mapper_airplane_distance_to_y")
 
 centers_airplane4 = airplane_mapper4.centers_of_patches
 using Distances
@@ -486,7 +646,26 @@ using Ripserer
 
 result_airplane4 = ripserer(dist_matrix_airplane4)
 fig_airplane4 = plot(result_airplane4, title="Persistence Diagrams for Mapper Complex")
-savefig(fig_airplane4, "persistence_diagram_airplane4")
+savefig(fig_airplane4, "persistence_diagram_airplane_distance_to_y")
+
+# airplane_16, distance to average point
+
+airplane_avg_point = average_point(airplane)
+airplane_mapper5 = mapper(airplane, distance_points_to_line, balanced_cover, cluster_function, silhouette_method, p1 = airplane_avg_point, ϵ = 1.0, measurement_func = point_distances2)
+println("no_of_patches: ", length(airplane_mapper5.found_patches))
+mapper_airplane_plot5 = plot(airplane_mapper5)
+savefig(mapper_airplane_plot5, "mapper_airplane_distance_to_avg_point")
+
+centers_airplane5 = airplane_mapper5.centers_of_patches
+using Distances
+dist_matrix_airplane5 = pairwise(Euclidean(), centers_airplane5)
+using Ripserer
+
+result_airplane5 = ripserer(dist_matrix_airplane5)
+fig_airplane5 = plot(result_airplane5, title="Persistence Diagrams for Mapper Complex")
+savefig(fig_airplane5, "persistence_diagram_airplane_distance_to_avg_point")
+
+
 
 
 
